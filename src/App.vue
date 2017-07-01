f4f47af25bc68dcbe2ef24c4464057adf66d9f00
ygd <Koondik@hotmail.com>
2017-07-01 17:33:02 +0800

    creat a Vue2.0 framework

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="app">
+        <img src="./assets/logo.png">
+        <h1>{{msg}}</h1>
+        <secondcomponent></secondcomponent>
+        <firstcomponent></firstcomponent>
+        <ul>
+            <li><router-link to="/first">点我跳转到第一页</router-link></li>
+            <li><router-link to="/second">点我跳转到第二页</router-link></li>
+        </ul>
+        <router-view class="view"></router-view>
+    </div>
+</template>
+
+<script>
+    import firstcomponent from './component/firstcomponent.vue'
+    import secondcomponent from './component/secondcomponent.vue'
+    export default {
+        name : 'app',
+        data () {
+            return {
+                msg : 'Hello App!'
+            }
+        },
+        components:{ firstcomponent , secondcomponent }
+    }
+</script>
+
+<style lang="scss">
+  body{
+      font-family:Helvetica , sans-serif;
+  }
+</style>
---
+++ src/component/secondcomponent.vue
@@ -0,0 +1,62 @@
+<template>
+    <div id="secondcomponent">
+        <h1>I am another page</h1>
+        <p> 感谢 <a href="https://github.com/showonne">showonne</a>大神的技术指导</p>
+        <el-card class="box-card">
+            <div slot="header" class="clearfix">
+                <h1 style="line-height: 36px; color: #20A0FF">豆瓣电影排行榜</h1>
+            </div>
+            <div v-for="article in articles" class="text item">
+                {{article.title}}
+            </div>
+        </el-card>
+    </div>
+</template>
+<script type="text/javascript">
+    import axios from 'axios'
+    export default{
+        data(){
+            return {
+                author: "微信公众号 jinkey-love",
+                articles: [],
+            }
+        },
+        mounted: function() {
+            this.$http.jsonp('https://api.douban.com/v2/movie/top250?count=10', {}, {
+                headers: {
+
+                },
+                emulateJSON: true
+            }).then(function(response) {
+                // 这里是处理正确的回调
+                this.articles = response.data.subjects
+                // this.articles = response.data["subjects"] 也可以
+            }, function(response) {
+                // 这里是处理错误的回调
+                console.log(response)
+            });
+        }
+    }
+</script>
+<style>
+    .text {
+        font-size: 14px;
+    }
+
+    .item {
+        padding: 18px 0;
+    }
+
+    .clearfix:before,
+    .clearfix:after {
+        display: table;
+        content: "";
+    }
+    .clearfix:after {
+        clear: both
+    }
+
+    .box-card {
+        width: 480px;
+    }
+</style>
